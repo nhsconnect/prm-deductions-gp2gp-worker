44598fb516dacb758510967249cfd6ae98a24d45
ewrobel-nhs <edyta.wrobel@nhs.net>
2021-03-01 15:23:49 +0000

    [PRMT-1529] Logging structure for bash scripts

--- run-worker.sh
+++ run-worker.sh
@@ -3,6 +3,16 @@
 # This script executes on docker container start.
 # It waits for MQ connection to be available before starting the application.
 
+#Structured logging
+NHS_SERVICE=gp2gp-worker
+timestamp() {
+  date +"%Y-%m-%d %H:%M:%S"
+}
+function jsonPrettify {
+  echo "{message: $1, level: $2, timestamp: `timestamp`, service: ${NHS_SERVICE}, environment: ${NHS_ENVIRONMENT} } "
+}
+
+
 MQ_CONNECTION_TIMEOUT=30
 
 MQ_HOST_1=$(echo $GP2GP_WORKER_MHS_QUEUE_URL_1 | awk -F "://" '{print $2}' |  awk -F ":" '{print $1}')
@@ -10,22 +20,22 @@ MQ_PORT_1=$(echo $GP2GP_WORKER_MHS_QUEUE_URL_1 | awk -F "://" '{print $2}' |  aw
 MQ_HOST_2=$(echo $GP2GP_WORKER_MHS_QUEUE_URL_2 | awk -F "://" '{print $2}' |  awk -F ":" '{print $1}')
 MQ_PORT_2=$(echo $GP2GP_WORKER_MHS_QUEUE_URL_2 | awk -F "://" '{print $2}' |  awk -F ":" '{print $2}')
 
-echo "Waiting for any MQ port to be open"
+jsonPrettify "Waiting for any MQ port to be open" INFO
 count=0
 while ! nc -z ${MQ_HOST_1} ${MQ_PORT_1} && ! nc -z ${MQ_HOST_2} ${MQ_PORT_2}; do
-  echo "Waiting for MQ at ${MQ_HOST_1}:${MQ_PORT_1} or ${MQ_HOST_2}:${MQ_PORT_2}"
+  jsonPrettify "Waiting for MQ at ${MQ_HOST_1}:${MQ_PORT_1} or ${MQ_HOST_2}:${MQ_PORT_2}" INFO
   sleep 2
   ((count++))
   if [ "${MQ_CONNECTION_TIMEOUT}" -le $count ]; then
-    echo "Timed-out waiting for MQ connection at ${MQ_HOST_1}:${MQ_PORT_1} or ${MQ_HOST_2}:${MQ_PORT_2}"
+    jsonPrettify "Timed-out waiting for MQ connection at ${MQ_HOST_1}:${MQ_PORT_1} or ${MQ_HOST_2}:${MQ_PORT_2}" WARN
     exit 5
   fi
 done
 
-echo "MQ is available. Waiting 5 seconds before connecting"
+jsonPrettify "MQ is available. Waiting 5 seconds before connecting" INFO
 sleep 5
 # After tcp port is open, it takes a moment for server to be ready
 
-echo "MQ is available. Starting worker"
+jsonPrettify "MQ is available. Starting worker" INFO
 set -e
 exec node worker.js
