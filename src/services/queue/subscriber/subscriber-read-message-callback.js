a89fe76f6e363c7e112275f1b277baffb132726c
aiwatko <oktawia.kata1@nhs.net>
2021-03-17 16:57:49 +0000

    [PRMT-1556] Remove span creation on message handling callback

--- src/services/queue/subscriber/subscriber-read-message-callback.js
+++ src/services/queue/subscriber/subscriber-read-message-callback.js
@@ -1,18 +1,12 @@
-import { context, setSpan } from '@opentelemetry/api';
 import { subscriberOnMessageCallback } from './subscriber-on-message-callback';
 import { logInfo, logError } from '../../../config/logging';
-import { tracer } from '../../../config/tracing';
 
 export const subscriberReadMessageCallback = channel => (err, messageStream) => {
-  const messageSpan = tracer.startSpan('messageSpan', context.active());
-  context.with(setSpan(context.active(), messageSpan), () => {
-    logInfo('Subscriber has Received Message');
-    if (err) {
-      logError(err);
-      return;
-    }
+  logInfo('Subscriber has Received Message');
+  if (err) {
+    logError(err);
+    return;
+  }
 
-    messageStream.readString('UTF-8', subscriberOnMessageCallback(channel, messageStream));
-  });
-  messageSpan.end();
+  messageStream.readString('UTF-8', subscriberOnMessageCallback(channel, messageStream));
 };
